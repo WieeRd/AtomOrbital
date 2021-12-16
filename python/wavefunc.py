ea4fa0cc36fb4360c70e3e8136ab1583cc038ec4
WieeRd_laptop <wowluke@naver.com>
2021-12-16 21:44:17 +0900

    Added get_wavefunc() using sympy.lambdify

--- python/wavefunc.py
+++ python/wavefunc.py
@@ -1,5 +1,6 @@
 import sympy
 from sympy.physics import hydrogen
+from typing import Any, Callable
 
 
 name = "spdf"
@@ -8,38 +9,135 @@ orientation = [
         0: "",
     },
     {
-        0: "z",
-        +1: "x",
-        -1: "y",
+        0: "_z",
+        +1: "_x",
+        -1: "_y",
     },
     {
-        0: "z2",
-        +1: "xz",
-        -1: "yz",
-        +2: "xy",
-        -2: "x2−y2",
+        0: "_z2",
+        +1: "_xz",
+        -1: "_yz",
+        +2: "_xy",
+        -2: "_x2−y2",
     },
     {
-        0: "z3",
-        +1: "xz2",
-        -1: "yz2",
-        +2: "xyz",
-        -2: "z(x2−y2)",
-        +3: "x(x2−3y2)",
-        -3: "y(3x2−y2)",
+        0: "_z3",
+        +1: "_xz2",
+        -1: "_yz2",
+        +2: "_xyz",
+        -2: "_z(x2−y2)",
+        +3: "_x(x2−3y2)",
+        -3: "_y(3x2−y2)",
     },
 ]
 
 
 def orbital_name(n: int, l: int, m: int) -> str:
     """
-    Generate orbital name based on quantum numbers
-    ex) (2,1,1) -> 2p_x
+    Generate orbital's name based on quantum numbers.
+
+    Parameters
+    ----------
+    n, l, m : `int`
+        Quantom numbers of the orbital
+
+    Returns
+    -------
+    `str`
+        The orbital's name. e.g. `orbital_name`(2,1,1) -> '2p_x'
+
+    Raises
+    ------
+    ValueError
+        When invalid quantum numbers are given
+    NotImplementedError
+        Currently only supported upto f orbital
     """
-    return f"{n}{name[l]}_{orientation[l][m]}"
+    if n < 1:
+        raise ValueError("'n' must be positive integer")
+    if not (n > l):
+        raise ValueError("'n' must be greater than 'l'")
+    if not (abs(m) <= l):
+        raise ValueError("|'m'| must be less or equal to 'l'")
+    if not (len(orientation) > l):
+        raise NotImplementedError("Currently only supported upto f orbital")
+    return f"{n}{name[l]}{orientation[l][m]}"
 
 
-class WaveFunc:
+# TODO: test performance
+# opt 1. transform first & pass to spherical
+# opt 2. use transformed Psi(x, y, z)
+# opt 3. numpy array operation
+
+# TODO: what about imaginary parts of Psi?
+
+
+def get_wavefunc(
+    n: int,
+    l: int,
+    m: int,
+    *,
+    Z: int = 1,
+    abs_sqrd: bool = False,
+    orthogonal: bool = False,
+    **options,
+) -> Callable[[Any, Any, Any], Any]:
+    """
+    Generate wave function of atomic orbital
+
+    Parameters
+    ----------
+    n, l, m : `int`
+        Quantom numbers of the orbital
+    Z : `int`
+        Atomic number. Default is set to 1 (Hydrogen)
+    abs_sqrd : `bool`
+        If `True`, absolute square of wave function (probability density) is used
+    orthogonal : `bool`
+        If `True`, returned function will take (x, y, z) instead of (r, theta, phi)
+    **options : `dict`
+        These kwargs will be passed to `sympy.lambdify()`
+
+    Returns
+    -------
+    Callable[[`float`, `float`, `float`], `float`]
+        Function that calculates Psi(r, theta, phi)
+
+    Notes
+    -----
+    Generated wave functions only returns real part
+    """
+    r = sympy.Symbol("r", real=True, positive=True)
+    theta = sympy.Symbol("theta", real=True)
+    phi = sympy.Symbol("phi", real=True)
+
+    # XXX: hydrogen.Psi_nlm() uses order [r, phi, theta] (why??)
+    expr = hydrogen.Psi_nlm(n, l, m, r, phi, theta, Z)
+    if abs_sqrd:
+        expr = expr * sympy.conjugate(expr)  # probability density
+    else:
+        expr = sympy.re(expr)  # only take the real part
+    assert isinstance(expr, sympy.Expr)
+
+    if not orthogonal:
+        return sympy.lambdify([r, theta, phi], expr, **options)
+
+    x = sympy.Symbol("x", real=True)
+    y = sympy.Symbol("y", real=True)
+    z = sympy.Symbol("z", real=True)
+
+    expr = expr.subs(
+        [
+            (r, x * x + y * y + z * z),
+            (theta, sympy.acos(z / y)),
+            (phi, sympy.atan(y / x)),
+        ]
+    )
+
+    return sympy.lambdify([x, y, z], expr, **options)
+
+
+class Psi_nlm:
     def __init__(self, n: int, l: int, m: int, *, Z: int = 1, prec: int = 6):
         self.n = n
         self.l = l
@@ -52,8 +150,8 @@ class WaveFunc:
         self.theta = sympy.Symbol("theta", real=True)
         self.phi = sympy.Symbol("phi", real=True)
 
-        sympy.lambdify  # TODO: this & numpy would make the code a lot faster
-        self.expr = hydrogen.Psi_nlm(n, l, m, self.r, self.theta, self.phi, Z)
+        # XXX: Psi_nlm uses order (r, phi, theta) for some reason
+        self.expr = hydrogen.Psi_nlm(n, l, m, self.r, self.phi, self.theta, Z)
 
     def __repr__(self) -> str:
         n, l, m = self.n, self.l, self.m
@@ -61,7 +159,7 @@ class WaveFunc:
 
     def __str__(self) -> str:
         n, l, m = self.n, self.l, self.m
-        return f"Psi({orbital_name(n, l, m)})"
+        return f"{self.__class__.__name__}({orbital_name(n, l, m)})"
 
     def spherical(self, r, theta, phi):
         subs = {self.r: r, self.theta: theta, self.phi: phi}
@@ -75,11 +173,7 @@ class WaveFunc:
         return self.expr.evalf(self.prec, subs=subs)
 
 
-class Orbital(WaveFunc):
+class PDF_nlm(Psi_nlm):
     def __init__(self, n: int, l: int, m: int, Z: int = 1, prec: int = 6):
         super().__init__(n, l, m, Z=Z, prec=prec)
         self.expr = self.expr * sympy.conjugate(self.expr)
-
-    def __str__(self) -> str:
-        n, l, m = self.n, self.l, self.m
-        return f"PDF({orbital_name(n, l, m)})"
